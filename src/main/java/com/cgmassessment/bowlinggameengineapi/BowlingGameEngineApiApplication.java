567e8f81ee202422518e06e2f923251a455fea2a
Francesco Mancini <Francesco.Mancini95@gmail.com>
2021-06-27 16:54:27 +0200

    Initialized Spring Application
    First commit with initialization of a Spring Application

---
+++ src/main/java/com/cgmassessment/bowlinggameengineapi/BowlingGameEngineApiApplication.java
@@ -0,0 +1,13 @@
+package com.cgmassessment.bowlinggameengineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingGameEngineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingGameEngineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cgmassessment/bowlinggameengineapi/BowlingGameEngineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgmassessment.bowlinggameengineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingGameEngineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
